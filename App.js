ba2144e835db7ba08418785167910ed0f7e3a991
David Balderston <dbalders@gmail.com>
2019-06-14 07:02:15 -0700

    added more pages and started strats

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import styles from './styles'
 // import { HomeScreen } from './HomeScreen';
 // import { ProfileScreen } from './ProfileScreen';
 
@@ -33,31 +34,57 @@ class HomeScreen extends React.Component {
 			<React.Fragment>
 				<Button
 					title="Mirage"
-					onPress={() => navigate('Map', { name: 'Mirage' })}
+					onPress={() => navigate('OffDef', { name: 'Mirage' })}
 				/>
 				<Button
 					title="Inferno"
-					onPress={() => navigate('Map', { name: 'Inferno' })}
+					onPress={() => navigate('OffDef', { name: 'Inferno' })}
 				/>
 				<Button
 					title="Dust2"
-					onPress={() => navigate('Map', { name: 'Dust2' })}
+					onPress={() => navigate('OffDef', { name: 'Dust2' })}
 				/>
 				<Button
 					title="Nuke"
-					onPress={() => navigate('Map', { name: 'Nuke' })}
+					onPress={() => navigate('OffDef', { name: 'Nuke' })}
 				/>
 				<Button
 					title="Train"
-					onPress={() => navigate('Map', { name: 'Train' })}
+					onPress={() => navigate('OffDef', { name: 'Train' })}
 				/>
 				<Button
 					title="Overpass"
-					onPress={() => navigate('Map', { name: 'Overpass' })}
+					onPress={() => navigate('OffDef', { name: 'Overpass' })}
 				/>
 				<Button
 					title="Vertigo"
-					onPress={() => navigate('Map', { name: 'Vertigo' })}
+					onPress={() => navigate('OffDef', { name: 'Vertigo' })}
+				/>
+			</React.Fragment>
+		);
+	}
+}
+
+class OffDefScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Offense or Defense',
+	};
+	render() {
+		const { navigate } = this.props.navigation;
+		const navigation = this.props
+		const mapName = this.props.navigation.getParam('name')
+		return (
+			<React.Fragment>
+				<Text style={styles.welcome}>
+					Offense or Defense?
+				</Text>
+				<Button
+					title="Offense"
+					onPress={() => navigate('Map', { side: 'Offense', map: mapName })}
+				/>
+				<Button
+					title="Defense"
+					onPress={() => navigate('Map', { side: 'Defense', map: mapName })}
 				/>
 			</React.Fragment>
 		);
@@ -72,27 +99,28 @@ class MapScreen extends React.Component {
 		const { navigate } = this.props.navigation;
 		const navigation = this.props
 		// console.log(navigate)
-		const mapName = this.props.navigation.getParam('name')
+		const mapName = this.props.navigation.getParam('map')
+		const mapSide = this.props.navigation.getParam('side')
 		return (
 			<React.Fragment>
-				<Button
-					title={mapName}
-				/>
+				<Text style={styles.welcome}>
+					{mapName}
+				</Text>
 				<Button
 					title="Pistol"
-					onPress={() => navigate('Strats', { strat: 'Pistol' })}
+					onPress={() => navigate('Strats', { strat: 'Pistol', map: mapName, side: mapSide })}
 				/>
 				<Button
 					title="Full Buy"
-					onPress={() => navigate('Strats', { strat: 'Full Buy' })}
+					onPress={() => navigate('Strats', { strat: 'Full Buy', map: mapName, side: mapSide })}
 				/>
 				<Button
 					title="Force Buy"
-					onPress={() => navigate('Strats', { strat: 'Force Buy' })}
+					onPress={() => navigate('Strats', { strat: 'Force Buy', map: mapName, side: mapSide })}
 				/>
 				<Button
 					title="Save"
-					onPress={() => navigate('Strats', { strat: 'Save' })}
+					onPress={() => navigate('Strats', { strat: 'Save', map: mapName, side: mapSide })}
 				/>
 			</React.Fragment>
 		);
@@ -104,21 +132,67 @@ class StratsScreen extends React.Component {
 		title: 'Strats',
 	};
 	render() {
+		const strats = {
+			mirage: require('./strats/mirage.json'),
+			inferno: require('./strats/inferno.json'),
+			cache: require('./strats/cache.json'),
+			dust2: require('./strats/dust2.json'),
+			overpass: require('./strats/overpass.json'),
+			nuke: require('./strats/nuke.json'),
+			train: require('./strats/train.json'),
+			vertigo: require('./strats/vertigo.json'),
+		}
+
 		const { navigate } = this.props.navigation;
-		const navigation = this.props
-		const stratName = this.props.navigation.getParam('strat')
+		const navigation = this.props;
+		const stratName = this.props.navigation.getParam('strat');
+		const mapName = this.props.navigation.getParam('map').toLowerCase();
+		const mapSide = this.props.navigation.getParam('side');
+		const stratData = strats[mapName];
+		var stratSelectedData = [];
+		for (var i = 0; i < stratData.length; i++) {
+			if ((stratData[i].Buy === stratName) && (stratData[i].Side === mapSide)) {
+				stratSelectedData.push(stratData[i])
+			}
+		}
+
+		const stratSelectedLength = stratSelectedData.length;
+		if (stratSelectedLength === 0) {
+			stratSelectedData.push({
+				"Buy": "No Strats",
+				"Start": "No Strats",
+				"Mid": "No Strats",
+				"Smokes": [],
+				"Event": "No Strats",
+				"Round": 0,
+				"Team": "No Strats",
+				"Side": "No Strats"
+			})
+			selectedStratsLength = 1;
+		}
+		const randomNumber = Math.floor(Math.random() * stratSelectedLength);
+		const stratSelected = stratSelectedData[randomNumber]
+
 		return (
 			<React.Fragment>
-				<Button
-					title={stratName}
-				/>
-			</React.Fragment>
+				<Text style={styles.welcome}>
+					{stratName} ({mapSide})
+				</Text>
+				<View style={styles.container, styles.flexWrapper}>
+					<Text style={styles.center, styles.strats}><Text style={{ fontWeight: 'bold' }}>Start:</Text> {stratSelected.Start}</Text>
+			<Text style={styles.center, styles.strats}> <Text style={{ fontWeight: 'bold' }}>Middle:</Text> { stratSelected.Mid }</Text >
+				<Text style={styles.center, styles.strats}><Text style={{ fontWeight: 'bold' }}>Smokes:</Text> {stratSelected.Smokes}</Text>
+				<Text style={styles.center, styles.strats}><Text style={{ fontWeight: 'bold' }}>Team:</Text> {stratSelected.Team}</Text>
+				<Text style={styles.center, styles.strats}><Text style={{ fontWeight: 'bold' }}>Event:</Text> {stratSelected.Event} - Round {stratSelected.Round}</Text>
+				</View >
+			</React.Fragment >
 		);
 	}
 }
 
 const MainNavigator = createStackNavigator({
 	Home: { screen: HomeScreen },
+	OffDef: { screen: OffDefScreen },
 	Map: { screen: MapScreen },
 	Strats: { screen: StratsScreen },
 });
@@ -140,21 +214,21 @@ export default App;
 //   }
 // }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
-});
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		justifyContent: 'center',
+// 		alignItems: 'center',
+// 		backgroundColor: '#F5FCFF',
+// 	},
+// 	welcome: {
+// 		fontSize: 20,
+// 		textAlign: 'center',
+// 		margin: 10,
+// 	},
+// 	instructions: {
+// 		textAlign: 'center',
+// 		color: '#333333',
+// 		marginBottom: 5,
+// 	},
+// });
---
+++ styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    // UNIVERSAL CSS
+    container: {
+        flex: 1,
+        margin: 20
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    flexWrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap'
+    },
+    flexContent: {
+        flex: 3
+    },
+    flex: {
+        display: 'flex'
+    },
+    flexVertical: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    flexOne: {
+        flex: 1
+    },
+    flexWrap: {
+        flexWrap: 'wrap'
+    },
+    flexCenter: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    center: {
+        textAlign: 'center',
+        margin: 'auto',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    },
+    hide: {
+        display: 'none',
+    },
+    noMargin: {
+        margin: 0,
+    },
+
+    // Strats css
+    strats: {
+        width: '100%',
+        margin: 10
+    }
+})
